024ce8a9d3acdc5a08b7f45bb2e13bab756d422f
pranavc1 <pranavchandiramani1@gmail.com>
2026-07-01 14:23:59 +0530

    exclude cron routes from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -36,5 +36,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|auth/callback).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|auth/callback|api/cron).*)'],
 }
